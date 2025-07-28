c302a04dbaa563a170d2570075ece031c06d711d
Hao Le <leduchao77g1@gmail.com>
2025-07-28 14:25:48 +0700

    remove table, column name attribute in entities

--- src/Modules/User/HatChao.Modules.User.Domain/Entities/User.cs
+++ src/Modules/User/HatChao.Modules.User.Domain/Entities/User.cs
@@ -1,23 +1,22 @@
 ﻿using HatChao.Modules.User.Domain.ValueObjects;
-using System.ComponentModel.DataAnnotations.Schema;
+using HatChao.SharedKernel.Domain.Abstractions;
 
 namespace HatChao.Modules.User.Domain.Entities;
 
-[Table("users")]
-public class User : BaseEntity
+public class User : BaseEntity<Guid>
 {
-    [Column("username")]
     public string Username { get; set; } = null!;
 
-    [Column("email")]
     public string Email { get; set; } = null!;
 
-    [Column("hashed_password")]
     public string HashedPassword { get; set; } = null!;
 
-    public User() {}
+    public User()
+    {
+        Id = Guid.NewGuid();
+    }
 
-    public User(string username, string email, string plainPassword)
+    public User(string username, string email, string plainPassword) : this()
     {
         Username = username;
         Email = ValueObjects.Email.Create(email);
---
+++ src/SharedKernel/HatChao.SharedKernel/Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HatChao.SharedKernel.Domain.Abstractions;
+
+public abstract class BaseEntity<TId>
+{
+    [Key]
+    public TId Id { get; protected set; } = default!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; } = false;
+}
